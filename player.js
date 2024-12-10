86aac23c5a736f9319528a7042d7d02e0918dd35
kevin-link <ryuzu1772@gmail.com>
2024-12-11 00:09:16 +0800

    12.10 musicPlay

---
+++ player.js
@@ -0,0 +1,8 @@
+
+const play = document.getElementById(play)
+
+play.addEventListener('click', musicPlay = (e) => {
+    let state = 0
+    let audio = document.createElement('Audio');
+    audio.setAttribute("src",  )
+} )
